8c985166c2ab2b277f212c29069aa780f0faa5aa
Nayan Pathak <pathaknayan8582@gmail.com>
2025-07-22 03:09:19 +0530

    add basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 import { useState } from 'react'
 import './App.css'
+import {RequestAirdrop} from './Airdrop';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (  
     <>
-      <div>
-        Hello Dapp
-        
-      </div>
+     <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+            { /* The `ConnectionProvider` provides the connection to the Solana cluster. */ }
+            <WalletProvider wallets={[]} autoConnect>
+                <WalletModalProvider>
+                    <WalletMultiButton /> 
+                    <RequestAirdrop />
+          
+                    { /* Your app's components go here, nested within the context providers. */ }
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+      
     </>
   )
 }
